edfe5bdb7bcacde46eecc049ef68b2a2821dd8fe
semgoryachev <semgoryachev@gmail.com>
2025-08-17 09:54:43 +0300

    add comments

--- cycles.go
+++ cycles.go
@@ -3,10 +3,12 @@ package main
 import f "fmt"
 
 func main() {
+	//simple cycle
 	for i := 1; i <= 5; i++ {
 		f.Println(i)
 	}
 
+	//cycle with range
 	numbers := []int{1, 2, 3, 4, 5}
 	for index, value := range numbers {
 		f.Printf("Index: %d, Value: %d\n", index, value)
